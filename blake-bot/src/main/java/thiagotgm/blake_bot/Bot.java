9677fd26a63f53c0eb467d6e8d3367da365a17d6
ThiagoTGM <thiagotgmgaming@gmail.com>
2016-12-28 03:15:47 -0200

    Added basic message receiving (echoes user and exiting).

--- blake-bot/src/main/java/thiagotgm/blake_bot/Bot.java
+++ blake-bot/src/main/java/thiagotgm/blake_bot/Bot.java
@@ -8,8 +8,12 @@ import sx.blah.discord.api.events.EventSubscriber;
 import sx.blah.discord.handle.impl.events.DisconnectedEvent;
 import sx.blah.discord.handle.impl.events.MessageReceivedEvent;
 import sx.blah.discord.handle.impl.events.ReadyEvent;
+import sx.blah.discord.handle.obj.IChannel;
+import sx.blah.discord.handle.obj.IMessage;
 import sx.blah.discord.handle.obj.Status;
 import sx.blah.discord.util.DiscordException;
+import sx.blah.discord.util.MessageBuilder;
+import sx.blah.discord.util.MissingPermissionsException;
 import sx.blah.discord.util.RateLimitException;
 
 import java.util.concurrent.CompletableFuture;
@@ -53,6 +57,33 @@ public class Bot {
         }
         log.info( "*** Discord bot armed ***" );
         
+    }
+    
+    @EventSubscriber
+    public void onMessageReceivedEvent( MessageReceivedEvent event ) {
+
+        IMessage message = event.getMessage(); // Gets the message from the event object NOTE: This is not the content of the message, but the object itself
+        IChannel channel = message.getChannel(); // Gets the channel in which this message was sent.
+
+        if ( message.getContent().equals( "!exit" ) ) {
+            terminate();
+            return;
+        }
+        
+        try {
+            // Builds (sends) and new message in the channel that the original message was sent with the content of the original message.
+            new MessageBuilder(this.client).withChannel(channel).withContent(message.getContent()).build();
+        } catch (RateLimitException e) { // RateLimitException thrown. The bot is sending messages too quickly!
+            System.err.print("Sending messages too quickly!");
+            e.printStackTrace();
+        } catch (DiscordException e) { // DiscordException thrown. Many possibilities. Use getErrorMessage() to see what went wrong.
+            System.err.print(e.getErrorMessage()); // Print the error message sent by Discord
+            e.printStackTrace();
+        } catch (MissingPermissionsException e) { // MissingPermissionsException thrown. The bot doesn't have permission to send the message!
+            System.err.print("Missing permissions for channel!");
+            e.printStackTrace();
+        }
+
     }
 
     @EventSubscriber
