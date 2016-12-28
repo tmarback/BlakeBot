83a58a1d23f57f0cac449caa8d212cea60eca517
Thiago Marback <tmarback@ucsd.edu>
2016-12-28 01:20:28 -0200

    Started Maven Project.

---
+++ blake-bot/src/main/java/thiagotgm/blake_bot/App.java
@@ -0,0 +1,13 @@
+package thiagotgm.blake_bot;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
