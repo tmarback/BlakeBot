9b3dce5dc496d273e4081f6ff12421011c15ff06
ThiagoTGM <thiagotgmgaming@gmail.com>
2017-07-10 00:02:08 -0300

    Added license to project (LGPL 3.0).

--- blakebot-module-status/src/main/java/com/github/thiagotgm/blakebot/module/status/PingCommand.java
+++ blakebot-module-status/src/main/java/com/github/thiagotgm/blakebot/module/status/PingCommand.java
@@ -1,3 +1,20 @@
+/*
+ * This file is part of BlakeBot.
+ *
+ * BlakeBot is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * BlakeBot is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with BlakeBot. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package com.github.thiagotgm.blakebot.module.status;
 
 import java.util.List;
