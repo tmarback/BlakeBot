6cdea7edb0331ec2c7d274fc1ad03ab61299f1aa
ThiagoTGM <thiagotgmgaming@gmail.com>
2017-07-28 12:00:01 -0300

    Made a class to track uptime.

---
+++ blakebot-module-status/src/main/java/com/github/thiagotgm/blakebot/module/status/Time.java
@@ -0,0 +1,255 @@
+/*
+ * This file is part of BlakeBot.
+ *
+ * BlakeBot is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * BlakeBot is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with BlakeBot. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.github.thiagotgm.blakebot.module.status;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Class that encapsulates an amount of time.
+ *
+ * @version 1.0
+ * @author ThiagoTGM
+ * @since 2017-01-31
+ */
+public class Time {
+    
+    private final long time;
+    
+    private boolean trimZeroes;
+    private boolean includeSeconds;
+    private boolean includeMillis;
+    
+    private static final int DAYS = 0;
+    private static final int HOURS = 1;
+    private static final int MINUTES = 2;
+    private static final int SECONDS = 3;
+    private static final int MILLIS = 4;
+    private static final int FIELDS = 5;
+    private static final String[] LABELS_LONG = { " days", " hours",
+            " minutes", " seconds ", " milliseconds" };
+    private static final String SEPARATOR_LONG = ", ";
+    private static final String[] LABELS_SHORT = { "d", "h", "m", "s", "ms" };
+    private static final String SEPARATOR_SHORT = " | ";
+    
+    /**
+     * Creates a new instance that represents a certain time amount.
+     *
+     * @param time The time to be represented by the instance.
+     */
+    public Time( long time ) {
+        
+        this.time = time;
+        
+        this.trimZeroes = false;
+        this.includeSeconds = false;
+        this.includeMillis = false;
+        
+    }
+    
+    /**
+     * Creates a new instance that is a copy of a given instance.
+     * String conversion settings are preserved.
+     *
+     * @param o Time object to be copied.
+     */
+    public Time( Time o ) {
+        
+        this.time = o.time;
+        
+        this.trimZeroes = o.trimZeroes;
+        this.includeSeconds = o.includeSeconds;
+        this.includeMillis = o.includeMillis;
+        
+    }
+    
+    /**
+     * Parses the time value into days, hours, minutes, seconds, milliseconds.
+     * 
+     * @return An array with each parsed value, in the order defined above.
+     */
+    private long[] parseTime() {
+        
+        long remaining = time;
+        long[] parsed = new long[FIELDS];
+        
+        parsed[DAYS] = TimeUnit.MILLISECONDS.toDays( remaining );
+        remaining -= TimeUnit.DAYS.toMillis( parsed[DAYS] );
+        parsed[HOURS] = TimeUnit.MILLISECONDS.toHours( remaining );
+        remaining -= TimeUnit.HOURS.toMillis( parsed[HOURS] );
+        parsed[MINUTES] = TimeUnit.MILLISECONDS.toMinutes( remaining );
+        remaining -= TimeUnit.MINUTES.toMillis( parsed[MINUTES] );
+        parsed[SECONDS] = TimeUnit.MILLISECONDS.toSeconds( remaining );
+        remaining -= TimeUnit.SECONDS.toMillis( parsed[SECONDS] );
+        parsed[MILLIS] = remaining;
+        
+        return parsed;
+        
+    }
+    
+    /**
+     * Returns the amount of time represented by this in milliseconds.
+     * 
+     * @return The time represented, in milliseconds.
+     */
+    public long getTotalTime() {
+        
+        return time;
+        
+    }
+    
+    /**
+     * Returns the amount of time represented by this, as an array with parsed
+     * values.
+     * 
+     * @return The parsed time value.<br>
+     *         Is returned in the form of an array with 5 values:<p>
+     *         index 0 - Days;<br>
+     *         index 1 - Hours;<br>
+     *         index 2 - Minutes;<br>
+     *         index 3 - Seconds;<br>
+     *         index 4 - Milliseconds.
+     */
+    public long[] getTime() {
+        
+        return parseTime();
+        
+    }
+    
+    /**
+     * Sets whether to cut off leading zero values when converting to string.
+     * 
+     * @param trim Whether to trim leading zeroes.
+     * @see #toString( boolean )
+     */
+    public void trimZeroes( boolean trim ) {
+        
+        this.trimZeroes = trim;
+        
+    }
+    
+    /**
+     * Sets whether to include seconds in the string value.
+     * 
+     * @param trim Whether to include seconds.
+     * @see #toString( boolean )
+     */
+    public void includeSeconds( boolean include ) {
+        
+        this.includeSeconds = include;
+        
+    }
+    
+    /**
+     * Sets whether to include milliseconds in the string value.
+     * OBS: If seconds are set to not be included, milliseconds will not
+     * be included either.
+     * 
+     * @param trim Whether to include milliseconds.
+     * @see #toString( boolean )
+     */
+    public void includeMillis( boolean include ) {
+
+        this.includeMillis = include;
+        
+    }
+    
+    /**
+     * Obtains the string that represents this time value, in long format.<br>
+     * Same as calling toString( true ).
+     *
+     * @return The string that represents this time value.
+     * @see #toString( boolean )
+     */
+    public String toString() {
+        
+        return toString( true );
+        
+    }
+    
+    /**
+     * Obtains the string that represents this time value.
+     * <p>
+     * Will use the long (DD days, HH hours, MM minutes, SS seconds,
+     * MS milliseconds) or short (DDd | HHh | MMm | SSs | MSms) format depending
+     * on the value of the argument received.
+     * <p>
+     * If includeMillis is set to false, will not include the amount of milliseconds.<br>
+     * If includeSeconds is set to false, will not include the amount of seconds or
+     * milliseconds.<br>
+     * If trimZeroes is set to true, any leftmost unit that has value 0 will be ignored
+     * (for example, 0 days 0 hours 20 minutes will be returned as 20 minutes). If it is
+     * set to false, all values will be printed. If set to true but all values (that are
+     * set to be included are 0, returns a 0 value of the leftmost unit.
+     * <p>
+     * By default, trimZeroes, includeSeconds, and includeMillis are set to false.
+     *
+     * @param longVersion If true, uses the long format. Else, uses the short format.
+     * @return The string that represents this time value.
+     * @see #trimZeroes( boolean )
+     * @see #includeSeconds( boolean )
+     * @see #includeMillis( boolean )
+     */
+    public String toString( boolean longVersion ) {
+        
+        long[] time = parseTime();
+        
+        // Remove low units if necessary.
+        int last;
+        if ( !includeSeconds ) {
+            last = MINUTES;
+        } else if ( !includeMillis ) {
+            last = SECONDS;
+        } else {
+            last = MILLIS;
+        }
+        
+        // Trims leading zeroes if necessary.
+        int first = DAYS;
+        if ( trimZeroes ) {
+            while ( ( first < last ) && ( time[first] == 0 ) ) {
+                
+                first++;
+                
+            }
+        }
+        
+        // Chooses long or short format.
+        String[] labels;
+        String separator;
+        if ( longVersion ) {
+            labels = LABELS_LONG;
+            separator = SEPARATOR_LONG;
+        } else {
+            labels = LABELS_SHORT;
+            separator = SEPARATOR_SHORT;
+        }
+        
+        // Builds the string.
+        String value = "";
+        for ( int i = first; i < last; i++ ) {
+            
+            value += time[i] + labels[i] + separator;
+            
+        }
+        value += time[last] + labels[last];
+        
+        return value;
+        
+    }
+
+}
---
+++ blakebot-module-status/src/main/java/com/github/thiagotgm/blakebot/module/status/UptimeTracker.java
@@ -0,0 +1,379 @@
+/*
+ * This file is part of BlakeBot.
+ *
+ * BlakeBot is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * BlakeBot is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with BlakeBot. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.github.thiagotgm.blakebot.module.status;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import sx.blah.discord.api.events.Event;
+import sx.blah.discord.api.events.EventSubscriber;
+import sx.blah.discord.handle.impl.events.ReadyEvent;
+import sx.blah.discord.handle.impl.events.shard.DisconnectedEvent;
+import sx.blah.discord.handle.impl.events.shard.ResumedEvent;
+
+/**
+ * Keeps track of uptime and downtime stats for the bot.
+ *
+ * @version 1.0
+ * @author ThiagoTGM
+ * @since 2017-07-28
+ */
+public class UptimeTracker {
+    
+    private static final Logger LOG = LoggerFactory.getLogger( UptimeTracker.class );
+    
+    private static final File UPTIME_FILE = Paths.get( "uptimes.log" ).toFile();
+    private static final File CONNECTION_FILE = Paths.get( "connection.log" ).toFile();
+    private static final String CONNECTION_LOG_SEPARATOR = " : ";
+
+    private volatile long connectTime;
+    private final TimeData uptimes;
+    
+    private final Writer connectionOutput;
+    
+    /**
+     * Initializes a new instance.
+     */
+    public UptimeTracker() {
+        
+        connectTime = -1;
+        
+        boolean logUptimes = true; // TODO: Make this a setting
+        boolean logConnection = true;
+        
+        uptimes = new TimeData( "uptime", ( logUptimes ) ? UPTIME_FILE : null );
+        
+        if ( logConnection ) { // Should log connections.
+            Writer output = null;
+            LOG.info( "Logging connection events." );
+            try { // Try to get output file.
+                output = new FileWriter( CONNECTION_FILE );
+            } catch ( IOException e ) {
+                LOG.error( "Could not open connection log file.", e );
+                output = null;
+            } finally {
+                connectionOutput = output;
+            }
+        } else { // Do not log.
+            connectionOutput = null;
+        }
+        
+    }
+    
+    /**
+     * Logs a connection event, if an output was specified on construction.
+     *
+     * @param event The event to be logged.
+     */
+    private void logConnectionEvent( Event event ) {
+        
+        if ( connectionOutput != null ) {
+            StringBuilder builder = new StringBuilder();
+            builder.append( System.currentTimeMillis() );
+            builder.append( CONNECTION_LOG_SEPARATOR );
+            if ( event instanceof DisconnectedEvent ) {
+                builder.append( "Disconnected" );
+                builder.append( CONNECTION_LOG_SEPARATOR );
+                builder.append( ( (DisconnectedEvent) event ).getReason() );
+            } else if ( event instanceof ReadyEvent ) {
+                builder.append( "Connected" );
+            } else if ( event instanceof ResumedEvent ) {
+                builder.append( "Reconnected" );
+            } else {
+                builder.append( "<?>" );
+            }
+            try {
+                connectionOutput.write( builder.toString() );
+            } catch ( IOException e ) {
+                LOG.error( "Could not write to connection log file." );
+            }
+        }
+        
+    }
+    
+    /**
+     * Detects that the bot was disconnected.
+     *
+     * @param event The event fired.
+     */
+    @EventSubscriber
+    public synchronized void disconnected( DisconnectedEvent event ) {
+        
+        LOG.debug( "Disconnected by {}.", event.getReason() );
+        logConnectionEvent( event );
+        
+        long uptime = System.currentTimeMillis() - connectTime;
+        
+        uptimes.recordTime( uptime );
+        
+        if ( LOG.isInfoEnabled() ) {
+            LOG.info( "Disconnected after " + new Time( uptime ).toString( false ) );
+        }
+        
+        connectTime = -1;
+        
+    }
+    
+    /**
+     * Detects that the bot was connected.
+     *
+     * @param event The event fired.
+     */
+    @EventSubscriber
+    public synchronized void connected( ReadyEvent event ) {
+        
+        connectTime = System.currentTimeMillis();
+        logConnectionEvent( event );
+        
+    }
+    
+    /**
+     * Detects that the bot was reconnected.
+     *
+     * @param event The event fired.
+     */
+    @EventSubscriber
+    public synchronized void resumed( ResumedEvent event ) {
+        
+        connectTime = System.currentTimeMillis();
+        logConnectionEvent( event );
+        
+    }
+    
+    /**
+     * Calculates the current uptime of the bot.
+     *
+     * @return The current uptime, in milliseconds. If not currently connected, returns 0.
+     */
+    private long currentUptime() {
+        
+        return ( connectTime != -1 ) ? System.currentTimeMillis() - connectTime : 0;
+        
+    }
+    
+    /**
+     * Retrieves the current uptime of the bot.
+     *
+     * @return The current uptime. If not currently connected, returns a time interval of 0.
+     */
+    public synchronized Time getCurrentUptime() {
+        
+        return new Time( currentUptime() );
+        
+    }
+    
+    /**
+     * Retrieves the total uptime of the bot.
+     *
+     * @return The total uptime (including the current one).
+     */
+    public synchronized Time getTotalUptime() {
+        
+        return new Time( uptimes.getTotal() + currentUptime() );
+        
+    }
+    
+    public synchronized Time getMinimumUptime() {
+        
+        return uptimes.getMinimum();
+        
+    }
+    
+    public synchronized Time getMaximumUptime() {
+        
+        return uptimes.getMaximum();
+        
+    }
+    
+    public synchronized Time getMeanUptime() {
+        
+        return uptimes.getMean();
+        
+    }
+    
+    public synchronized Time getMedianUptime() {
+        
+        return uptimes.getMedian();
+        
+    }
+    
+    public int getDisconnectAmount() {
+        
+        return uptimes.getAmount();
+        
+    }
+    
+    /**
+     * Class that keeps track of a set of time intervals.
+     *
+     * @version 1.0
+     * @author ThiagoTGM
+     * @since 2017-07-28
+     */
+    private class TimeData {
+        
+        private final String errorString;
+        private final Writer output;
+        
+        private final List<Long> sortedTimes;
+        private long total;
+        
+        /**
+         * Constructs a data set with the given name, that logs data to the given output file.
+         *
+         * @param name The name of the data set.
+         * @param outputFile The file to log data to. If null, will not be logged.
+         */
+        public TimeData( String name, File outputFile ) {
+            
+            this.errorString = "Could not write to " + name + " log file.";
+
+            if ( outputFile != null ) { // Log to a file.
+                Writer output = null;
+                LOG.info( "Logging {}.", name );
+                try { // Try to get output file.
+                    output = new FileWriter( outputFile );
+                } catch ( IOException e ) {
+                    LOG.error( "Could not open " + name + " log file.", e );
+                    output = null;
+                } finally {
+                    this.output = output;
+                }
+            } else { // No file to log to.
+                this.output = null;
+            }
+            
+            sortedTimes = new ArrayList<>();
+            total = 0;
+            
+        }
+        
+        /**
+         * Records a time interval into this data set.
+         *
+         * @param time The time interval to be recorded.
+         */
+        public void recordTime( long time ) {
+            
+            /* Output to file if necessary */
+            if ( output != null ) {
+                try {
+                    output.write( String.valueOf( time ) );
+                } catch ( IOException e ) {
+                    LOG.error( errorString, e );
+                }
+            }
+            
+            /* Add to sorted time list */
+            int i = 0;
+            while ( ( i < sortedTimes.size() ) && ( time > sortedTimes.get( i ) ) ) {
+                
+                i++; // Find position for the time being inserted.
+                    
+            }
+            sortedTimes.add( i, time );
+            
+            /* Add to total time */
+            total += time;
+            
+        }
+        
+        /**
+         * Retrieves the total amount of time recorded in this data set.
+         *
+         * @return The total time, in milliseconds.
+         */
+        public long getTotal() {
+            
+            return total;
+            
+        }
+        
+        /**
+         * Retrieves the smallest time interval recorded.
+         *
+         * @return The smallest time.
+         */
+        public Time getMinimum() {
+            
+            return new Time( sortedTimes.get( 0 ) );
+            
+        }
+        
+        /**
+         * Retrieves the largest time interval recorded.
+         *
+         * @return The largest time.
+         */
+        public Time getMaximum() {
+            
+            return new Time( sortedTimes.get( sortedTimes.size() - 1 ) );
+            
+        }
+        
+        /**
+         * Retrieves the mean time interval of this data set.
+         *
+         * @return The mean.
+         */
+        public Time getMean() {
+            
+            return new Time( total / sortedTimes.size() );
+            
+        }
+        
+        /**
+         * Retrieves the median time interval of this data set.
+         *
+         * @return The median.
+         */
+        public Time getMedian() {
+            
+            int middle = sortedTimes.size() / 2;
+            long median;
+            if ( ( sortedTimes.size() % 2 ) == 1 ) { // Odd number of times.
+                median = sortedTimes.get( middle );
+            } else { // Even number of times.
+                median = ( sortedTimes.get( middle - 1 ) + sortedTimes.get( middle ) ) / 2;
+            }
+            return new Time( median );
+            
+        }
+        
+        /**
+         * Retrieves the amount of time intervals stored in this data set.
+         *
+         * @return The amount of time intervals.
+         */
+        public int getAmount() {
+            
+            return sortedTimes.size();
+            
+        }
+        
+    }
+
+}
