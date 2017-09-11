ccc64c0fd2c92355a19a4abd21d95460a30089e8
ThiagoTGM <thiagotgmgaming@gmail.com>
2017-09-11 00:44:23 -0300

    Wrote utility methods to write/read entire XML documents.

--- blakebot-common/src/main/java/com/github/thiagotgm/blakebot/common/utils/Utils.java
+++ blakebot-common/src/main/java/com/github/thiagotgm/blakebot/common/utils/Utils.java
@@ -20,11 +20,20 @@ package com.github.thiagotgm.blakebot.common.utils;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.NotSerializableException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import java.io.OutputStream;
 import java.io.Serializable;
 import java.util.Base64;
+
+import javax.xml.stream.XMLInputFactory;
+import javax.xml.stream.XMLOutputFactory;
+import javax.xml.stream.XMLStreamConstants;
+import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.XMLStreamReader;
+import javax.xml.stream.XMLStreamWriter;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -127,6 +136,46 @@ public abstract class Utils {
             LOG.error( "Decoded object is not of the expected type.", e );
         }
         return null;
+        
+    }
+    
+    /* XML writing/reading methods */
+    
+    /**
+     * Writes an XML document to a stream, where the content of the document is the XMLElement
+     * given.
+     *
+     * @param out The stream to write to.
+     * @param content The content of the document to be written.
+     * @throws XMLStreamException if an error is encountered while writing.
+     */
+    public static void writeXMLDocument( OutputStream out, XMLElement content ) throws XMLStreamException {
+        
+        XMLStreamWriter outStream = XMLOutputFactory.newFactory().createXMLStreamWriter( out );
+        outStream.writeStartDocument();
+        content.write( outStream );
+        outStream.writeEndDocument();
+        
+    }
+    
+    /**
+     * Reads an XML document from a stream, where the content of the document is to be read
+     * by the given XMLElement.
+     * <p>
+     * If there is any content after what is read by the given XMLElement, that extra content
+     * is ignored. This means that the stream will always be read until the end of the document
+     * is found.
+     *
+     * @param in The stream to read to.
+     * @param content The element that will read the document's content.
+     * @throws XMLStreamException if an error is encountered while reading.
+     */
+    public static void readXMLDocument( InputStream in, XMLElement content ) throws XMLStreamException {
+        
+        XMLStreamReader inStream = XMLInputFactory.newFactory().createXMLStreamReader( in );
+        while ( inStream.next() != XMLStreamConstants.START_ELEMENT ) {} // Skip comments.
+        content.read( inStream );
+        while ( inStream.hasNext() ) { inStream.next(); } // Go to end of document.
         
     }
 
