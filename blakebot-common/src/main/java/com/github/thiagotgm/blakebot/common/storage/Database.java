6591c96cedbb16ed64091c3dcd68a61370412f9d
ThiagoTGM <thiagotgmgaming@gmail.com>
2018-07-25 12:50:34 -0700

    Added some extra documentation in Database.

--- blakebot-common/src/main/java/com/github/thiagotgm/blakebot/common/storage/Database.java
+++ blakebot-common/src/main/java/com/github/thiagotgm/blakebot/common/storage/Database.java
@@ -43,9 +43,11 @@ public abstract class Database implements Closeable {
 	 * 
 	 * @param treeName The name of the tree.
 	 * @return The tree.
-	 * @throws NullPointerException If the tree name is null.
+	 * @throws NullPointerException if the tree name is null.
+	 * @throws IllegalStateException if the database is already closed.
 	 */
-	public Tree<String,String> getDataTree( String treeName ) throws NullPointerException {
+	public Tree<String,String> getDataTree( String treeName )
+			throws NullPointerException, IllegalStateException {
 		
 		return getTranslatedDataTree( treeName, new StringTranslator(), new StringTranslator() );
 		
@@ -57,11 +59,12 @@ public abstract class Database implements Closeable {
 	 * @param treeName The name of the tree.
 	 * @param keyTranslator The translator to use to convert keys into strings.
 	 * @return The tree.
-	 * @throws NullPointerException If the tree name or the translator is null.
+	 * @throws NullPointerException if the tree name or the translator is null.
+	 * @throws IllegalStateException if the database is already closed.
 	 * @param <K> The type of the keys that define connections on the tree.
 	 */
 	public <K> Tree<K,String> getKeyTranslatedDataTree( String treeName,
-			Translator<K> keyTranslator ) throws NullPointerException {
+			Translator<K> keyTranslator ) throws NullPointerException, IllegalStateException {
 		
 		return getTranslatedDataTree( treeName, keyTranslator, new StringTranslator() );
 		
@@ -73,11 +76,12 @@ public abstract class Database implements Closeable {
 	 * @param treeName The name of the tree.
 	 * @param valueTranslator The translator to use to convert values into strings.
 	 * @return The tree.
-	 * @throws NullPointerException If the tree name or the translator is null.
+	 * @throws NullPointerException if the tree name or the translator is null.
+	 * @throws IllegalStateException if the database is already closed.
 	 * @param <V> The type of the values stored in the tree.
 	 */
 	public <V> Tree<String,V> getValueTranslatedDataTree( String treeName,
-			Translator<V> valueTranslator ) throws NullPointerException {
+			Translator<V> valueTranslator ) throws NullPointerException, IllegalStateException {
 		
 		return getTranslatedDataTree( treeName, new StringTranslator(), valueTranslator );
 		
@@ -90,21 +94,25 @@ public abstract class Database implements Closeable {
 	 * @param keyTranslator The translator to use to convert keys into strings.
 	 * @param valueTranslator The translator to use to convert values into strings.
 	 * @return The tree.
-	 * @throws NullPointerException If the tree name or either of the translators is null.
+	 * @throws NullPointerException if the tree name or either of the translators is null.
+	 * @throws IllegalStateException if the database is already closed.
 	 * @param <K> The type of the keys that define connections on the tree.
 	 * @param <V> The type of the values stored in the tree.
 	 */
 	public abstract <K,V> Tree<K,V> getTranslatedDataTree( String treeName,
-			Translator<K> keyTranslator, Translator<V> valueTranslator ) throws NullPointerException;
+			Translator<K> keyTranslator, Translator<V> valueTranslator )
+			throws NullPointerException, IllegalStateException;
 	
 	/**
 	 * Obtains a data map backed by this database that maps strings to strings.
 	 * 
 	 * @param mapName The name of the map.
 	 * @return The map.
-	 * @throws NullPointerException If the map name is null.
+	 * @throws NullPointerException if the map name is null.
+	 * @throws IllegalStateException if the database is already closed.
 	 */
-	public Map<String,String> getDataMap( String mapName ) throws NullPointerException {
+	public Map<String,String> getDataMap( String mapName )
+			throws NullPointerException, IllegalStateException {
 		
 		return getTranslatedDataMap( mapName, new StringTranslator(), new StringTranslator() );
 		
@@ -116,11 +124,12 @@ public abstract class Database implements Closeable {
 	 * @param mapName The name of the map.
 	 * @param keyTranslator The translator to use to convert keys into strings.
 	 * @return The map.
-	 * @throws NullPointerException If the map name or the translator is null.
+	 * @throws NullPointerException if the map name or the translator is null.
+	 * @throws IllegalStateException if the database is already closed.
 	 * @param <K> The type of the keys that define connections on the map.
 	 */
 	public <K> Map<K,String> getKeyTranslatedDataMap( String mapName,
-			Translator<K> keyTranslator ) throws NullPointerException {
+			Translator<K> keyTranslator ) throws NullPointerException, IllegalStateException {
 		
 		return getTranslatedDataMap( mapName, keyTranslator, new StringTranslator() );
 		
@@ -133,10 +142,11 @@ public abstract class Database implements Closeable {
 	 * @param valueTranslator The translator to use to convert values into strings.
 	 * @return The map.
 	 * @throws NullPointerException If the map name or the translator is null.
+	 * @throws IllegalStateException if the database is already closed.
 	 * @param <V> The type of the values stored in the map.
 	 */
 	public <V> Map<String,V> getValueTranslatedDataMap( String mapName,
-			Translator<V> valueTranslator ) throws NullPointerException {
+			Translator<V> valueTranslator ) throws NullPointerException, IllegalStateException {
 		
 		return getTranslatedDataMap( mapName, new StringTranslator(), valueTranslator );
 		
@@ -150,18 +160,21 @@ public abstract class Database implements Closeable {
 	 * @param valueTranslator The translator to use to convert values into strings.
 	 * @return The map.
 	 * @throws NullPointerException If the map name or either of the translators is null.
+	 * @throws IllegalStateException if the database is already closed.
 	 * @param <K> The type of the keys that define connections on the map.
 	 * @param <V> The type of the values stored in the map.
 	 */
 	public abstract <K,V> Map<K,V> getTranslatedDataMap( String mapName,
-			Translator<K> keyTranslator, Translator<V> valueTranslator ) throws NullPointerException;
+			Translator<K> keyTranslator, Translator<V> valueTranslator )
+			throws NullPointerException, IllegalStateException;
 	
 	/**
 	 * Retrieves all the data trees in this database.
 	 * 
 	 * @return The data trees of this database, keyed by their names.
+	 * @throws IllegalStateException if the database is already closed.
 	 */
-	protected abstract Map<String,Tree<String,String>> getDataTrees();
+	protected abstract Map<String,Tree<String,String>> getDataTrees() throws IllegalStateException;
 	
 	/**
 	 * Retrieves the names of the parameters required for {@link #load(List)}.
@@ -190,8 +203,9 @@ public abstract class Database implements Closeable {
 	 * is discarded).
 	 * 
 	 * @param db The database to load into this one.
+	 * @throws IllegalStateException if either this or the given database is already closed.
 	 */
-	void load( Database db ) {
+	void copyData( Database db ) throws IllegalStateException {
 		
 		for ( Map.Entry<String,Tree<String,String>> tree : db.getDataTrees().entrySet() ) {
 			
