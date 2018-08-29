e554bec00ee90eab884e6093f6c0a494ff30d83e
ThiagoTGM <thiagotgmgaming@gmail.com>
2018-08-29 12:17:26 -0700

    Added a convenience method to Translator for use in Map.
    Method attempts to translate a general Object. Saves having to recode
    the try-catch for ClassCastExceptions.

--- blakebot-common/src/main/java/com/github/thiagotgm/blakebot/common/storage/Translator.java
+++ blakebot-common/src/main/java/com/github/thiagotgm/blakebot/common/storage/Translator.java
@@ -22,7 +22,7 @@ import java.io.IOException;
 /**
  * Translates (converts) objects of a certain type to a string and vice versa.
  * 
- * @version 1.0
+ * @version 1.1
  * @author ThiagoTGM
  * @since 2018-07-16
  * @param <T> The type of object to be translated.
@@ -37,6 +37,27 @@ public interface Translator<T> {
 	 * @throws IOException if an error was encountered while encoding.
 	 */
 	String encode( T obj ) throws IOException;
+	
+	/**
+	 * Attempts the given object into a String format. If the given
+	 * object is not of the type that can be translated by this Translator,
+	 * returns <tt>null</tt>.
+	 * 
+	 * @param obj The object to be encoded.
+	 * @return The String encoding of the object, or <tt>null</tt> if the given
+	 *         object is not of the supported type.
+	 * @throws IOException if an error was encountered while encoding.
+	 */
+	@SuppressWarnings("unchecked")
+	default String encodeObj( Object obj ) throws IOException {
+		
+		try {
+			return encode( (T) obj ); // Attempt to translate.
+		} catch ( ClassCastException e ) {
+			return null; // Not the correct type.
+		}
+		
+	}
 	
 	/**
 	 * Restores an object from a String created using {@link #encode(Object)}.
